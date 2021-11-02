5dace6012c2a48049351d0ca26c71d3a09ef9246
usersina <benna.anis2@gmail.com>
2021-11-02 15:24:32 +0100

    add base layout & phone call

---
+++ Intents/app/src/main/java/com/example/intents/ImplicitManagerActivity.java
@@ -0,0 +1,63 @@
+package com.example.intents;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class ImplicitManagerActivity extends AppCompatActivity {
+    EditText numOrUrlInp;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_implicit_manager);
+        numOrUrlInp = findViewById(R.id.numOrUrlInp);
+    }
+
+
+    public void onNumberCall(View view) {
+        String myInput = numOrUrlInp.getText().toString();
+        if(myInput.isEmpty()) return;
+        Log.d("IMPLICIT", "onNumberCall: Call phone!");
+        int permissionCheck = ContextCompat.checkSelfPermission(
+            ImplicitManagerActivity.this,
+            Manifest.permission.CALL_PHONE
+        );
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                ImplicitManagerActivity.this,
+                new String[]{Manifest.permission.CALL_PHONE}, 123
+            );
+        } else {
+            Intent callIntent = new Intent(
+                Intent.ACTION_CALL,
+                Uri.parse("tel:" + myInput)
+            );
+            startActivity(callIntent);
+        }
+    }
+
+    public void onPhoneDial(View view) {
+    }
+
+    public void onWebsiteOpen(View view) {
+        String myInput = numOrUrlInp.getText().toString();
+        if(myInput.isEmpty()) return;
+    }
+
+    public void onAppManage(View view) {
+    }
+
+    public void onWifiManage(View view) {
+    }
+}
