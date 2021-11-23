b08eb05696a1c965f622ca5754fdd2d571a0d68f
usersina <benna.anis2@gmail.com>
2021-11-23 16:07:24 +0100

    add menu click handlers

--- Menus/app/src/main/java/com/example/menus/MainActivity.java
+++ Menus/app/src/main/java/com/example/menus/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.menus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu3, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.activity_item:
+                startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+                break;
+            case R.id.settings_item:
+                startActivity(new Intent(Settings.ACTION_SETTINGS));
+                break;
+            case R.id.languages_item:
+                startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
+                break;
+            case R.id.exit_item:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
