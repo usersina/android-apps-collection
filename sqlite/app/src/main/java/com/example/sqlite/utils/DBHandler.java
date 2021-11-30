5f9016fff08afec3d96a017524946cef980b99f9
usersina <benna.anis2@gmail.com>
2021-11-30 10:27:42 +0100

    create db handler

--- sqlite/app/src/main/java/com/example/sqlite/utils/DBHandler.java
+++ sqlite/app/src/main/java/com/example/sqlite/utils/DBHandler.java
@@ -1,4 +1,28 @@
 package com.example.sqlite.utils;
 
-public class DBHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+
+public class DBHandler extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "products";
+
+    public DBHandler(Context context, SQLiteDatabase.CursorFactory factory) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE products (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS product;");
+        this.onCreate(db);
+    }
 }
