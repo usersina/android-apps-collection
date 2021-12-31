02f75939b66075ba1f207512869d539990df4f37
usersina <benna.anis2@gmail.com>
2021-12-31 12:29:19 +0100

    Add base menu

--- FirebaseGps/app/src/main/java/com/example/firebasegps/MainActivity.java
+++ FirebaseGps/app/src/main/java/com/example/firebasegps/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.firebasegps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
