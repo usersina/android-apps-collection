99af2bf4e7a93f8023b26c56a56657c6958781bf
usersina <benna.anis2@gmail.com>
2021-12-31 12:34:13 +0100

    Add handlers for items selection

--- FirebaseGps/app/src/main/java/com/example/firebasegps/MainActivity.java
+++ FirebaseGps/app/src/main/java/com/example/firebasegps/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.firebasegps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +22,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.my_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.products_item:
+                Log.d("MainActivityLog:", "Selecting products item!");
+                break;
+            case R.id.gps_item:
+                Log.d("MainActivityLog:", "Selecting gps item!");
+                break;
+            case R.id.exit_item:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
