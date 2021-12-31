814277142bdd6dfe335f7aa1e292eec9c7466ac6
usersina <benna.anis2@gmail.com>
2021-12-31 17:30:20 +0100

    Add base product item styles & ripple effect

--- FirebaseGps/app/src/main/java/com/example/firebasegps/MainActivity.java
+++ FirebaseGps/app/src/main/java/com/example/firebasegps/MainActivity.java
@@ -2,19 +2,65 @@ package com.example.firebasegps;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.firebasegps.adapters.ProductAdapter;
+import com.example.firebasegps.models.Product;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView productRv;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    ProductAdapter productAdapter;
+    ArrayList<Product> productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInAnonymously();
+        // mAuth.signOut();
+
+        productRv = findViewById(R.id.products_rv);
+//        productRv.setHasFixedSize(true);
+        productRv.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+
+        productList = new ArrayList<>();
+        productAdapter = new ProductAdapter(getApplicationContext(), productList);
+        productRv.setAdapter(productAdapter);
+
+        db = FirebaseFirestore.getInstance();
+        db.collection("products").get()
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document: task.getResult()) {
+                        Log.d("MainActivityOnCreate", document.getId() + " => " + document.getData());
+                        Log.d("MainActivityOnCreate", (String) document.getData().get("name"));
+                        Log.d("MainActivityOnCreate", String.valueOf(document.getData().get("price")));
+                        productList.add(new Product(String.valueOf(document.getData().get("name")), Double.valueOf((Long) document.getData().get("price"))));
+                    }
+                    productAdapter.notifyDataSetChanged();
+                } else {
+                    Log.w("MainActivityOnCreate: ", "Error getting documents!", task.getException());
+                }
+            });
+
     }
 
     @Override
--- FirebaseGps/app/src/main/java/com/example/firebasegps/models/Product.java
+++ FirebaseGps/app/src/main/java/com/example/firebasegps/models/Product.java
@@ -4,7 +4,7 @@ public class Product {
     String name;
     double price;
 
-    Product(String name, double price) {
+    public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
