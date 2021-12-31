04dc7f03c635c91b71d6975c0b6780a643a415fa
usersina <benna.anis2@gmail.com>
2022-01-01 00:40:15 +0100

    Make a gps tracker

--- FirebaseGps/app/src/main/java/com/example/firebasegps/MapsActivity.java
+++ FirebaseGps/app/src/main/java/com/example/firebasegps/MapsActivity.java
@@ -1,21 +1,42 @@
 package com.example.firebasegps;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.firebasegps.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+    final static int PERMISSION_ALL = 1;
+    final static String[] PERMISSIONS = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+    public static final String TAG = "MapsActivity: ";
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+
+    LocationManager locationManager;
+    MarkerOptions markerOptions;
+    Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +48,87 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        markerOptions = new MarkerOptions().position(new LatLng(0, 0)).title("Current Location");
+        if (Build.VERSION.SDK_INT >= 23 && !isPermissionGranted()) {
+            requestPermissions(PERMISSIONS, PERMISSION_ALL);
+        } else requestLocation();
+        if (isLocationEnabled()) showAlert(1);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        marker = mMap.addMarker(markerOptions);
     }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        LatLng myCoordinates = new LatLng(location.getLatitude(), location.getLongitude());
+        Log.d(TAG, "onLocationChanged: " + myCoordinates.toString());
+        marker.setPosition(myCoordinates);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myCoordinates));
+    }
+
+    private void requestLocation() {
+        // -- Setup the preferred mode for getting the location
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        // -- Update the location every 10 seconds
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 10000, 10, this);
+    }
+
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private boolean isPermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG, "Permission is granted.");
+                return true;
+            } else {
+                Log.d(TAG, "Permission is not granted!");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void showAlert(final int status) {
+        String message, title, btnText;
+        if (status == 1) {
+            message = "Location settings are OFF, please enable them!";
+            title = "Enable location";
+            btnText = "Location Settings";
+        } else {
+            message = "Allow this app to access location settings!";
+            title = "Permission access";
+            btnText = "Grant";
+        }
+
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(getApplicationContext());
+        dialog.setCancelable(false);
+        dialog.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(btnText, (dialogInterface, i) -> {
+                if (status == 1) {
+                    startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                } else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        requestPermissions(PERMISSIONS, PERMISSION_ALL);
+                    }
+                }
+            })
+            .setNegativeButton("Cancel", (dialogInterface, i) -> finish());
+    }
+
 }
